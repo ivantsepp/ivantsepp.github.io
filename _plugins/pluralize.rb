cf10c2c13a0b99520ca53563aaeb47396bf2380c
Ivan Tse <ivan@paperlesspost.com>
2013-12-06 15:19:45 -0500

    Some more changes and added two plugins: pluralize and reading_time

---
+++ _plugins/pluralize.rb
@@ -0,0 +1,18 @@
+# https://github.com/bdesham/pluralize
+module Jekyll
+  module Pluralize
+
+    def pluralize(number, singular, plural=nil)
+      if number == 1
+        "#{number} #{singular}"
+      elsif plural == nil
+        "#{number} #{singular}s"
+      else
+        "#{number} #{plural}"
+      end
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Pluralize)
